cb98e1d0269fb1714a2c2df3906f642b7907bacf
Foxioo <foxioo2137@gmail.com>
2025-08-05 23:24:35 +0200

    sdada

--- Scripts/LastUpdate.js
+++ Scripts/LastUpdate.js
@@ -1,20 +1,44 @@
-
 fetch('https://api.github.com/repos/FoxiooOfficial/foxiooofficial.github.io/commits?per_page=1')
     .then(response => response.json())
     .then(data => {
         const commitDate = new Date(data[0].commit.author.date);
-        const formattedDate = commitDate.toLocaleDateString('en-GB', {
+
+        const formattedCommitDate = commitDate.toLocaleString('en-GB', {
             year: 'numeric',
             month: '2-digit',
             day: '2-digit',
             hour: '2-digit',
-            minute: '2-digit'
+            minute: '2-digit',
+            timeZone: 'Europe/Warsaw',
+            hour12: false
         });
-        document.getElementById('last-update').innerHTML = 
-        `Last update of the website:<br>
-        <strong>${formattedDate}</strong>
-        <br><br>
-        <i style="color: gray">(Information from GitHub)</i>`;
+
+        document.getElementById('last-update').innerHTML = `
+            Last update of the website:<br>
+            <strong>${formattedCommitDate}</strong>
+            <br>
+            <i style="color: gray; font-size: 12px;">(Information from GitHub)</i>
+            <br><br>
+            Current time:<br>
+            <strong id="live-clock"></strong>
+        `;
+
+        function updateClock() {
+            const nowInPoland = new Date().toLocaleString('en-GB', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                timeZone: 'Europe/Warsaw',
+                hour12: false
+            });
+            document.getElementById('live-clock').textContent = nowInPoland;
+        }
+
+        updateClock();
+        setInterval(updateClock, 1000);
     })
     .catch(error => {
         document.getElementById('last-update').innerHTML = 'Error loading update date.';
