02fcabecc39ddd431557b70a9d4431910c8bf53a
Foxioo <foxioo2137@gmail.com>
2025-11-30 18:49:50 +0100

    Tails7DocsOfficial!!!!

--- tails7-docs/config.js
+++ tails7-docs/config.js
@@ -7,29 +7,40 @@ const documentation = {
             { id: "whats-new-R1.0.1", title: "What's new", file: "pages/R1.0.1/whats-new.html" }
         ],
         "Overview": [
-            { id: "player-hitbox-R1.0.1", title: "Player Hitbox", file: "pages/R1.0.1/objects/player-hitbox.html" },
+            { id: "tails7-player-hitbox-R1.0.1", title: "Tails7 Player Hitbox", file: "pages/R1.0.1/objects/tails7-player-hitbox.html" },
+            { id: "tails7-info-object-R1.0.1", title: "Tails7 Info Object", file: "pages/R1.0.1/objects/tails7-info-object.html" },
+            { id: "tails7-render-ground-R1.0.1", title: "Tails7 Render Ground", file: "pages/R1.0.1/objects/tails7-render-ground.html" },
             { 
                 id: "tails7-mapper-object-R1.0.1", 
                 title: "Tails7 Mapper Object", 
                 file: "pages/R1.0.1/objects/tails7-mapper-object.html",
                 subcategories: [
-                    { id: "mapper-properties-R1.0.1", title: "Properties", file: "pages/R1.0.1/objects/mapper/mapper-properties.html" },
-                    { id: "mapper-methods-R1.0.1", title: "Methods", file: "pages/R1.0.1/objects/mapper/mapper-methods.html" },
-                    { id: "mapper-events-R1.0.1", title: "Events", file: "pages/R1.0.1/objects/mapper/mapper-events.html" }
+                    { id: "mapping-mode-R1.0.1", title: "Mapping Mode", file: "pages/R1.0.1/objects/mapper-object/mapping-mode.html" },
+                    { id: "mapping-screen-mode-R1.0.1", title: "Mapping Screen Mode", file: "pages/R1.0.1/objects/mapper-object/mapping-screen-mode.html" },
+                    { id: "distance-mode-R1.0.1", title: "Distance Mode", file: "pages/R1.0.1/objects/mapper-object/distance-mode.html" },
+                    { id: "scale-mode-R1.0.1", title: "Scale Mode", file: "pages/R1.0.1/objects/mapper-object/scale-mode.html" },
+                    { id: "layer-mode-R1.0.1", title: "Layer Mode", file: "pages/R1.0.1/objects/mapper-object/layer-mode.html" },
+                    { id: "visible-mode-R1.0.1", title: "Visible Mode", file: "pages/R1.0.1/objects/mapper-object/visible-mode.html" },
                 ]
             },
-            { id: "tails7-info-object-R1.0.1", title: "Tails7 Info Object", file: "pages/R1.0.1/objects/tails7-info-object.html" },
-            { id: "tails7-render-ground-R1.0.1", title: "Tails7 Render Ground", file: "pages/R1.0.1/objects/tails7-render-ground.html" },
             { id: "tails7-camera-object-R1.0.1", title: "Tails7 Camera Object", file: "pages/R1.0.1/objects/tails7-camera-object.html" },
             { id: "tails7-center-pointer-R1.0.1", title: "Tails7 Center Pointer", file: "pages/R1.0.1/objects/tails7-center-pointer.html" },
-            { id: "tails7-background-gradient-R1.0.1", title: "Tails7 Background Gradient", file: "pages/R1.0.1/objects/tails7-background-gradient.html" },
+            { 
+                id: "tails7-background-gradient-R1.0.1", 
+                title: "Tails7 Background Gradient", 
+                file: "pages/R1.0.1/objects/tails7-background-gradient.html",
+                subcategories: [
+                    { id: "tails7-external-background-gradient-R1.0.1", title: "External", file: "pages/R1.0.1/objects/gradient/external.html" },
+                    { id: "tails7-internal-background-gradient-R1.0.1", title: "Internal", file: "pages/R1.0.1/objects/gradient/internal.html" },
+                ]
+            },
             { id: "sprite-objects-R1.0.1", title: "Sprite Objects", file: "pages/R1.0.1/objects/sprite-objects.html" },
             { id: "polygon-objects-R1.0.1", title: "Polygon Objects", file: "pages/R1.0.1/objects/polygon-objects.html" },
             { id: "line-objects-R1.0.1", title: "Line Objects", file: "pages/R1.0.1/objects/line-objects.html" }
         ],
         "Aspirations": [
             { id: "tutorials-R1.0.1", title: "Tutorials", file: "pages/Global/tutorials.html" },
-            { id: "examples-R1.0.1", title: "Examples", file: "pages/R1.0.1/examples.html" },
+            { id: "examples-R1.0.1", title: "Examples", file:"pages/Global/examples.html"  },
         ],
         "Help": [
             { id: "report-an-issue-R1.0.1", title: "Report an issue", file: "pages/Global/report-an-issue.html" },
