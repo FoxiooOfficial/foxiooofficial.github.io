e5a8348d4eeea80aad585ea6a0153b5897e1111e
Foxioo <foxioo2137@gmail.com>
2026-04-18 20:32:26 +0200

    18.04.26

---
+++ script/LastUpdate.js
@@ -0,0 +1,45 @@
+fetch('https://api.github.com/repos/FoxiooOfficial/foxiooofficial.github.io/commits?per_page=1')
+    .then(response => response.json())
+    .then(data => {
+        const commitDate = new Date(data[0].commit.author.date);
+
+        const formattedCommitDate = commitDate.toLocaleString('en-GB', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            timeZone: 'Europe/Warsaw',
+            hour12: false
+        });
+
+        document.getElementById('info').innerHTML = `
+            Last update of the website:<br>
+            <strong>${formattedCommitDate}</strong>
+            <br>
+            <i style="color: gray; font-size: 12px;">(Information from GitHub)</i>
+            <br><br>
+            Current time:<br>
+            <strong id="live-clock"></strong>
+        `;
+
+        function updateClock() {
+            const nowDate = new Date().toLocaleString('en-GB', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: 'Europe/Warsaw',
+                hour12: false
+            });
+            document.getElementById('clock').textContent = nowDate;
+        }
+
+        updateClock();
+        setInterval(updateClock, 60000);
+    })
+    .catch(error => {
+        document.getElementById('info').innerHTML = 'Error loading update date.';
+        console.error('GitHub API error:', error);
+    });
---
+++ script/data.js
@@ -0,0 +1,35 @@
+const _Year_Copyright = "2025-2026";
+
+function Fun_Loaded() {
+    var _Switch_Style = document.getElementById("switch_style");
+    var _Parms = new URLSearchParams(window.location.search);
+    var _IsNoStyle = _Parms.get('style') === 'no';
+
+    if (!_IsNoStyle)
+    {
+        var _Link = document.createElement('link');
+        _Link.rel = 'stylesheet';
+        _Link.href = 'script/style.css';
+        document.head.appendChild(_Link);
+    }
+    else
+    {
+        _Switch_Style.innerHTML = '<b>Is this page too lightweight for your device?</b> <a href="?style=yes">View the HTML version with CSS</a> <i>(only if your browser supports it)</i>.';
+
+        var _Links = document.querySelectorAll('div.window-content ul li a');
+        for (var i = 0; i < _Links.length; i++)
+        {
+            var _Href = _Links[i].getAttribute('href');
+            if (_Href && _Href.indexOf('http') !== 0)
+            {
+                _Links[i].href = _Href + "?style=no";
+            }
+        }
+    }
+
+    /* Footer */
+    var _Footer = document.getElementById("footer_copyright");
+    _Footer.innerHTML = "<p>" + _Year_Copyright + " &copy; Foxioo<p>";
+}
+
+window.addEventListener('DOMContentLoaded', Fun_Loaded);
