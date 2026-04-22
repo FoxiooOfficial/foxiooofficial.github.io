cbbe60dd5b07ac03ac9e122694f36f644905146f
Foxioo <foxioo2137@gmail.com>
2026-04-23 00:32:32 +0200

    more compatibliy!

--- script/updates.js
+++ script/updates.js
@@ -1,27 +1,28 @@
-function Fun_Post_And_Updates() {
+function Fun_Post_And_Updates()
+{
     var _Updates = document.getElementById('updates');
-    
-    if (_Updates) {
-        _Updates.innerHTML = ""; 
+    if (!_Updates) return;
 
-        _Posts.forEach((item, idx) => {
-            var _Post = document.createElement('div');
-            _Post.className = 'updates-post';
+    var _Content = "";
 
-            var _img = "";
-            if (item.img && item.img !== "") {
-                _img = `<img src="${item.img}" alt="${item.imgalt}" style="max-width: 100%; height: auto; margin-top: 10px;">`;
-            }
-            var _PostID = _Posts.length - idx - 1;
+    for (var i = 0; i < _Posts.length; i++) {
+        var _item = _Posts[i];
+        var _PostID = _Posts.length - i - 1;
+        
+        var _img = "";
+        if (_item.img && _item.img !== "")
+        {
+            _img = '<img src="' + _item.img + '" alt="' + _item.imgalt + '" style="max-width: 100%; height: auto; margin-top: 10px;">';
+        }
 
-            _Post.innerHTML = `
-                <p><strong>${item.title}</strong> <em style="color: gray; font-size: 12px;">(${item.date} <strong>#${_PostID}</strong>)</em></p>
-                <p>${item.content}</p>
-                ${_img}
-                <hr>
-            `;
-
-            _Updates.appendChild(_Post);
-        });
+        _Content += '<div class="updates-post">' +
+                        '<p><strong>' + _item.title + '</strong> ' +
+                        '<em style="color: gray; font-size: 12px;">(' + _item.date + ' <strong>#' + _PostID + '</strong>)</em></p>' +
+                        '<p>' + _item.content + '</p>' +
+                        _img +
+                        '<hr>' +
+                     '</div>';
     }
+
+    _Updates.innerHTML = _Content;
 }
