b5158b89feabc14435a89f03e2d1cc8bed928c2d
Foxioo <foxioo2137@gmail.com>
2026-04-22 21:25:58 +0200

    post 22.04.26

--- script/posts.js
+++ script/posts.js
@@ -1,36 +1,43 @@
 const _Posts = [
-    {
+    { // 6
+        title: "important information",
+        date: "22.04.2026 21:25",
+        content: "I'm currently dealing with some health issues, so I'm putting work on fsp and tails7 r1.0.3 on hold for now :/",
+        img: "/media/posts/6/niko.jpg",
+        imgalt: "Niko plush"
+    },
+    { // 5
         title: "Super-duper (small) fixes!!!!",
         date: "20.04.2026 20:43",
         content: "For the lightweight version, i improved the page layout; the content now flows from top to bottom (instead of from the center)<br><br>ALSO a better awesome background for CSS!!1111!",
     },
-    {
+    { // 4
         title: ":O",
         date: "19.04.2026 22:40",
         content: "This site is now Suzu approved!",
         img: "/media/posts/4/suzu.webp",
         imgalt: "Suzu :O"
     },
-    {
+    { // 3
         title: "Added 'my-games.html'",
         date: "19.04.2026 20:47",
         content: "Oh, and a concept art from September 10, 2023 :P",
         img: "/media/posts/3/battle.png",
         imgalt: "oh no!1! The Robocat is attacking!!!"
     },
-    {
+    { // 2
         title: "The 'My Project' page is back!",
         date: "19.04.2026 19:42",
         content: "(The title says it all)",
     },
-    {
+    { // 1
         title: "New website design!",
         date: "19.04.2026 16:41",
         content: "Say hi to new website look!<br><br>The site is being built from scratch and is designed to run more smoothly (my computer used to lag ;w;) and work on old browsers :D",
         img: "/media/posts/0/artworks-000641319277-1qrgjn-t500x500.jpg",
         imgalt: "Senko-san!"
     },
-    {
+    { // 0
         title: "Test",
         date: "19.04.2026 16:20",
         content: "hello world",
